7b6b18f585147dcd89be1db897efc82ac2513979
SinduNagalingam <sindu1202@gmail.com>
2018-08-15 09:05:20 +0000

    Change block rewards

--- src/validation.cpp
+++ src/validation.cpp
@@ -1136,12 +1136,12 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 500 * COIN;
+    CAmount nSubsidy = 50 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
 
-     if(nHeight == 2)  
+     if(nHeight < 15)  
     {
-        nSubsidy = 83500000 * COIN;
+        nSubsidy = 5000000* COIN;
     }
     nSubsidy >>= halvings;
     return nSubsidy;
